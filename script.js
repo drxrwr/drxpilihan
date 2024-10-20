ce9b6d4198d43e2678b9c02e5774e8d85ec9ec79
drxrwr <iimabdur29@gmail.com>
2024-10-20 21:03:09 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Password yang benar
-    const correctPassword = 'passwordku123';
+    const correctPassword = 'drxrendi123';
   
     // Prompt untuk meminta kata sandi dari pengguna
     const userPassword = prompt('Masukkan kata sandi untuk mengakses website:');
@@ -8,11 +8,11 @@ document.addEventListener('DOMContentLoaded', function() {
     // Cek apakah kata sandi benar
     if (userPassword === correctPassword) {
       // Jika kata sandi benar, izinkan akses
-      alert('Sandi benar. Selamat datang!');
+      alert('WOW. otakmu encer! selamat datang');
       document.body.style.display = 'block'; // Menampilkan konten halaman
     } else {
       // Jika kata sandi salah, tolak akses
-      alert('Sandi salah. Anda tidak diperbolehkan mengakses halaman ini.');
+      alert('Hati-Hati,Sandi yang berulang kali akan dilaporkan.');
       document.body.style.display = 'none'; // Sembunyikan konten halaman
     }
   });
